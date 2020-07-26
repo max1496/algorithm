ca98e6933a87a0dd9787965862807d5dd5d6af1f
max1496 <max1495@naver.com>
2020-07-26 11:27:39 +0900

    구구단 input추가

--- 구구단.py
+++ 구구단.py
@@ -2,7 +2,7 @@ def multi(number):
     for i in range(1,10):
         print("{}*{} = {}".format(i,number, i*number))
 while True:
-    n= input()
+    n= input("숫자를 입력하세요>")
     if n == "exit":
         break
     elif n.isdigit()!=True:
